276bf129ee0cf5ce7de15f63fa37069af84ceacf
shahriyarshehab <kshahriyar28@gmail.com>
2023-05-25 03:40:57 +0600

    app.jsx edited

--- src/app.jsx
+++ src/app.jsx
@@ -18,15 +18,15 @@ export function App() {
       </div>
       <h1>Vite + Preact</h1>
       <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount((count) => count + 2)}>
+          count is {count} 
         </button>
         <p>
           Edit <code>src/app.jsx</code> and save to test HMR
         </p>
       </div>
       <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
+        Click on the Vite and Preact logos to learn more {count}
       </p>
     </>
   )
